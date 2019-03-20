4c5d9ae6c631cd9729f0639739935de675273e06
jupegarnica <jupegarnica@gmail.com>
2019-03-20 12:36:58 +0100

    GET /value working

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const fs = require( 'fs' );
 
-const port = Number( process.argv[ 2 ] ) || 3000;
+const port =  4000;
 
 
 // BODY PARSE TO JSON
@@ -13,13 +13,30 @@ app.use( express.json() );
 // enable CORS
 app.use( function ( req, res, next ) {
     res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Methods", "GET, PUT, POST, DELETE" );
     res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
     next();
 } );
 
 
 
+app.get('/value', (req,res) => {
 
+    const jsonString =  fs.readFileSync('./db.json','UTF-8');
+    const data = JSON.parse(jsonString);
+    res.json(data);
+
+})
+
+app.get( '/increment', ( req, res ) => {
+
+
+} )
+
+app.get( '/decrement', ( req, res ) => {
+
+
+} )
 
 
 
