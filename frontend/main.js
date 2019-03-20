34b307b1f14d0940692960cdeb9908c03811ee29
jupegarnica <jupegarnica@gmail.com>
2019-03-20 12:47:28 +0100

    get initial value from backend

--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,35 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 
+    // declarations
+    ///////////////
+
+    const counterNode = document.querySelector( '.counter' );
+
+    const updateCounterDOM = (value) => {
+        counterNode.innerText = String( value )
+    }
+
+    // get initial value from backend
+    fetch( 'http://localhost:4000/value' )
+        .then( response => response.json() )
+        .then( data => {
+            updateCounterDOM(data.counterValue )
+        } )
+
+    // Listeners
+    ////////////
 
 
-
+    // listener for incremente
     document.querySelector( 'header .increment' ).addEventListener( 'click', () => {
         // increment counter
 
-        let counterNode = document.querySelector( '.counter' );
 
         let num = Number( counterNode.innerText )
 
         let nextValue = num + 1;
         counterNode.innerText = nextValue;
 
-
     } )
-
 
 } )
